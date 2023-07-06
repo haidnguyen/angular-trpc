4e45b54a085e415466de1a54688d2de314a3fa63
Hai Nguyen <dinhhai281@gmail.com>
2023-07-06 23:00:19 +0700

    refactor(demo): rename styleClass to className for tailwind suggestion

--- libs/demo/ui-component/src/lib/input.directive.ts
+++ libs/demo/ui-component/src/lib/input.directive.ts
@@ -5,5 +5,5 @@ import { Directive, HostBinding } from '@angular/core';
   standalone: true,
 })
 export class InputDirective {
-  @HostBinding('class') styleClass = 'border-light text-default mb-4 rounded border border-solid px-6 py-3 text-xl';
+  @HostBinding('class') className = 'border-light text-default mb-4 rounded border border-solid px-6 py-3 text-xl';
 }
